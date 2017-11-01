6fbd9464b717daaed10e0832f112a8cca2a79699
sangbpark94 <sangbp1994@gmail.com>
2017-11-01 14:01:10 -0400

    added tables for group info and more groups sections in fellowship page

--- client/src/components/Fellowship.js
+++ client/src/components/Fellowship.js
@@ -46,7 +46,7 @@ class Fellowship extends Component {
         </div>
         <div className="fellowship-container">
           <Row className="fellowship-row">
-            <h1 className="fellowship-header"> Friends Gathering </h1>
+            <h1 className="fellowship-header">Friends Gathering </h1>
           </Row>
           <Row className="fellowship-row">
             <Col>
@@ -56,9 +56,18 @@ class Fellowship extends Component {
             </Col>
             <Col>
               <div className="fellowship-info">
-                <p><b>Time: </b> 8:00pm </p>
-                <p><b>Location: </b> Multiple Locations </p>
-                <p><b>Contact: </b> Pastor Jae </p>
+                <tr>
+                  <td><b>Time: </b></td>
+                  <td>Thurs. & Fri. 7pm</td>
+                </tr>
+                <tr>
+                  <td><b>Location: </b></td>
+                  <td>Pastor Jaes Humble Abode & Church</td>
+                </tr>
+                <tr>
+                  <td><b>Contact: </b></td>
+                  <td>Pastor Pio</td>
+                </tr>
               </div>
             </Col>
           </Row>
@@ -76,9 +85,10 @@ class Fellowship extends Component {
             </div>
           </Row>
         </div>
+
         <div>
           <Row className="fellowship-row">
-            <h1 className="fellowship-header"> Tee Class </h1>
+            <h1 className="fellowship-header">Tee Class </h1>
           </Row>
           <Row className="fellowship-row">
             <Col>
@@ -88,9 +98,18 @@ class Fellowship extends Component {
             </Col>
             <Col>
               <div className="fellowship-info">
-                <p><b>Time: </b> 8:00pm </p>
-                <p><b>Location: </b> Multiple Locations </p>
-                <p><b>Contact: </b> Pastor Jae </p>
+                <tr>
+                  <td><b>Time: </b></td>
+                  <td>Sunday 11am</td>
+                </tr>
+                <tr>
+                  <td><b>Location: </b></td>
+                  <td>Church</td>
+                </tr>
+                <tr>
+                  <td><b>Contact: </b></td>
+                  <td>Jason Park</td>
+                </tr>
               </div>
             </Col>
           </Row>
@@ -108,6 +127,133 @@ class Fellowship extends Component {
             </div>
           </Row>
         </div>
+
+        <div>
+          <Row className="fellowship-row">
+            <h1 className="fellowship-header">Prayer Meeting </h1>
+          </Row>
+          <Row className="fellowship-row">
+            <Col>
+              <div className="fellowship-slider">
+                <UncontrolledCarousel autoPlay={false} items={carouselItems2} />
+              </div>
+            </Col>
+            <Col>
+              <div className="fellowship-info">
+                <tr>
+                  <td><b>Time: </b></td>
+                  <td>Saturday 7pm</td>
+                </tr>
+                <tr>
+                  <td><b>Location: </b></td>
+                  <td>Church</td>
+                </tr>
+                <tr>
+                  <td><b>Contact: </b></td>
+                  <td>Lulu Lim</td>
+                </tr>
+              </div>
+            </Col>
+          </Row>
+          <Row className="fellowship-row">
+            <div className="fellowship-description">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Proin gravida aliquam semper. In laoreet urna lacus, nec varius
+              orci mattis sed. Integer porta non tortor et placerat. Nam mollis
+              eget lectus sed blandit. Morbi dolor lorem, rutrum in tellus ut,
+              congue vehicula ex. Aenean id lorem et diam tempor convallis ac ut
+              nunc. Donec eu augue eu enim laoreet vehicula sed ac ex.
+              Morbi facilisis lorem ante, sed commodo nisi lobortis vitae. Vivamus
+              sit amet tincidunt nibh. Nullam et nisl felis.
+              </p>
+            </div>
+          </Row>
+        </div>
+
+        <div>
+          <Row className="fellowship-row">
+            <h1 className="fellowship-header">Saturday Group</h1>
+          </Row>
+          <Row className="fellowship-row">
+            <Col>
+              <div className="fellowship-slider">
+                <UncontrolledCarousel autoPlay={false} items={carouselItems2} />
+              </div>
+            </Col>
+            <Col>
+              <div className="fellowship-info">
+                <tr>
+                  <td><b>Time: </b></td>
+                  <td>Saturday 5:30pm</td>
+                </tr>
+                <tr>
+                  <td><b>Location: </b></td>
+                  <td>Church</td>
+                </tr>
+                <tr>
+                  <td><b>Contact: </b></td>
+                  <td>Dr. Soo-Il Lee</td>
+                </tr>
+              </div>
+            </Col>
+          </Row>
+          <Row className="fellowship-row">
+            <div className="fellowship-description">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Proin gravida aliquam semper. In laoreet urna lacus, nec varius
+              orci mattis sed. Integer porta non tortor et placerat. Nam mollis
+              eget lectus sed blandit. Morbi dolor lorem, rutrum in tellus ut,
+              congue vehicula ex. Aenean id lorem et diam tempor convallis ac ut
+              nunc. Donec eu augue eu enim laoreet vehicula sed ac ex.
+              Morbi facilisis lorem ante, sed commodo nisi lobortis vitae. Vivamus
+              sit amet tincidunt nibh. Nullam et nisl felis.
+              </p>
+            </div>
+          </Row>
+        </div>
+
+        <div>
+          <Row className="fellowship-row">
+            <h1 className="fellowship-header">Parent Group</h1>
+          </Row>
+          <Row className="fellowship-row">
+            <Col>
+              <div className="fellowship-slider">
+                <UncontrolledCarousel autoPlay={false} items={carouselItems2} />
+              </div>
+            </Col>
+            <Col>
+              <div className="fellowship-info">
+                <tr>
+                  <td><b>Time: </b></td>
+                  <td>Monday</td>
+                </tr>
+                <tr>
+                  <td><b>Location: </b></td>
+                  <td>Church</td>
+                </tr>
+                <tr>
+                  <td><b>Contact: </b></td>
+                  <td>Dr. Soo-Il Lee</td>
+                </tr>
+              </div>
+            </Col>
+          </Row>
+          <Row className="fellowship-row">
+            <div className="fellowship-description">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Proin gravida aliquam semper. In laoreet urna lacus, nec varius
+              orci mattis sed. Integer porta non tortor et placerat. Nam mollis
+              eget lectus sed blandit. Morbi dolor lorem, rutrum in tellus ut,
+              congue vehicula ex. Aenean id lorem et diam tempor convallis ac ut
+              nunc. Donec eu augue eu enim laoreet vehicula sed ac ex.
+              Morbi facilisis lorem ante, sed commodo nisi lobortis vitae. Vivamus
+              sit amet tincidunt nibh. Nullam et nisl felis.
+              </p>
+            </div>
+          </Row>
+        </div>
+
       </div>
     )
   }
