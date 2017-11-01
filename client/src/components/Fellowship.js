16bdf423561da957609fa8856a6465a6c45c4a67
sangbpark94 <sangbp1994@gmail.com>
2017-11-01 13:02:56 -0400

    added intro for fellowship page and background

--- client/src/components/Fellowship.js
+++ client/src/components/Fellowship.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 import Slider from './Slider';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 import carousel4 from '../photos/carousel/carousel-4.jpg'
 import carousel5 from '../photos/carousel/carousel-5.jpg'
@@ -36,24 +37,33 @@ const carouselItems2 = [
 class Fellowship extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <Row>
+      <div className="fellowship-page-background">
+        <div className="fellowship-intro-container">
+          <p className="fellowship-intro-text">
+          "They devoted themselves to the apostles' teaching and to
+          fellowship, to the breaking of bread and to prayer" Acts 2:42 NIV
+          </p>
+        </div>
+        <div className="fellowship-container">
+          <Row className="fellowship-row">
+            <h1 className="fellowship-header"> Friends Gathering </h1>
+          </Row>
+          <Row className="fellowship-row">
             <Col>
-              <div className="testA">
+              <div className="fellowship-slider">
                 <Slider carouselItems={carouselItems1} />
               </div>
             </Col>
             <Col>
-              <div className="testB">
+              <div className="fellowship-info">
                 <p><b>Time: </b> 8:00pm </p>
                 <p><b>Location: </b> Multiple Locations </p>
                 <p><b>Contact: </b> Pastor Jae </p>
               </div>
             </Col>
           </Row>
-          <Row>
-            <div className="testC">
+          <Row className="fellowship-row">
+            <div className="fellowship-description">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Proin gravida aliquam semper. In laoreet urna lacus, nec varius
               orci mattis sed. Integer porta non tortor et placerat. Nam mollis
@@ -67,22 +77,25 @@ class Fellowship extends Component {
           </Row>
         </div>
         <div>
-          <Row>
+          <Row className="fellowship-row">
+            <h1 className="fellowship-header"> Tee Class </h1>
+          </Row>
+          <Row className="fellowship-row">
             <Col>
-              <div className="testA">
+              <div className="fellowship-slider">
                 <Slider carouselItems={carouselItems2} />
               </div>
             </Col>
             <Col>
-              <div className="testB">
+              <div className="fellowship-info">
                 <p><b>Time: </b> 8:00pm </p>
                 <p><b>Location: </b> Multiple Locations </p>
                 <p><b>Contact: </b> Pastor Jae </p>
               </div>
             </Col>
           </Row>
-          <Row>
-            <div className="testC">
+          <Row className="fellowship-row">
+            <div className="fellowship-description">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Proin gravida aliquam semper. In laoreet urna lacus, nec varius
               orci mattis sed. Integer porta non tortor et placerat. Nam mollis
