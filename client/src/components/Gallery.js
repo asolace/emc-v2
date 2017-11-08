b59a3c74838270c6284f4d1d670ada6fdae6b0b9
sangbpark94 <sangbp1994@gmail.com>
2017-11-08 17:23:27 -0500

    changed the layout of gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 import picA from '../photos/emcgallery/1.jpg'
 import picB from '../photos/emcgallery/2.jpg'
@@ -11,50 +12,28 @@ import picF from '../photos/emcgallery/6.JPG'
 const Gallery = () => {
   return (
     <div className="g-page">
+    
       <div className="g-header">Gallery</div>
       <br/>
-      <Row>
-        <div className="g-rowA-colA">
-          <Col>
-            <img className="g-picA"src={picA}></img>
-          </Col>
-        </div>
-        <div className="g-rowA-colB">
-          <Col>
-            <Row>
-              <Col>
-                <img className="g-picB"src={picB}></img>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <img className="g-picC"src={picC}></img>
-              </Col>
-            </Row>
-          </Col>
-        </div>
-      </Row>
-      <Row>
-        <div className="g-rowB-colA">
-          <Col>
-            <Row>
-              <Col>
-                <img className="g-picD"src={picD}></img>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <img className="g-picE"src={picE}></img>
-              </Col>
-            </Row>
-          </Col>
-        </div>
-        <div className="g-rowB-colB">
-          <Col>
-            <img className="g-picF"src={picF}></img>
-          </Col>
-        </div>
-      </Row>
+      <div className="g-posA g-rowA g-fadein one">
+        <img className="g-pic" src={picA}></img>
+      </div>
+      <div className="g-posA g-colB g-rowA g-fadein two">
+        <img className="g-pic" src={picB}></img>
+      </div>
+      <div className="g-posA g-colC g-rowA g-fadein three">
+        <img className="g-pic" src={picC}></img>
+      </div>
+      <div className="g-posA g-rowB g-fadein four">
+          <img className="g-pic" src={picD}></img>
+      </div>
+      <div className="g-posA g-colB g-rowB g-fadein five">
+        <img className="g-pic" src={picE}></img>
+      </div>
+      <div className="g-posA g-colC g-rowB g-fadein six">
+        <img className="g-pic" src={picF}></img>
+      </div>
+
     </div>
   )
 }
