c675d56cfb2620e1420f76151251779fa3cc79d8
Jack H <jackhe678@gmail.com>
2017-11-16 18:49:07 -0500

    making signup page. looks just like login

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import FaUser from 'react-icons/lib/fa/user';
+import FaLock from 'react-icons/lib/fa/lock';
 
 class SignUp extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  onSubmit = event => {
+    event.preventDefault()
+    console.log(this.state);
+  }
+
   render() {
-    return(
-        <div>
-          <Link to="/login">
-            <h1>The Sign-Up Page is Under Construction</h1>
-          </Link>
+    return (
+      <div className="la">
+        <div className="lb">
+          <div className="lc">
+            <div className="ld">
+              <p className="lp">"So we, though many, </p>
+              <p className="lp">are one body in Christ"</p>
+              <p className="lp">Romans 12:5 ESV</p>
+            </div>
+            <Form>
+              <FormGroup>
+                <div className="le">
+                  <div className="lm"></div>
+                  <Input className="ll" onChange={this.onChange} type="username" name="username" placeholder="Username" required/>
+                  <span className="lf"><FaUser /></span>
+                </div>
+              </FormGroup>
+              <FormGroup>
+                <div className="lg">
+                  <div className="ln"></div>
+                  <Input className="lo" onChange={this.onChange} type="password" name="password" placeholder="Password" required/>
+                  <span className="lh"><FaLock /></span>
+                </div>
+              </FormGroup>
+              <Button onClick={this.onSubmit} color="info"><div className="li">SignUp</div></Button>
+            </Form>
+            <p className="lj">Or <a href="/signup" className="lq">login</a></p>
+          </div>
         </div>
+
+        <div className="lk">
+          <p>Here at EMC, we value membership and relationships very highly.</p>
+          <p>If you would like to be a part of our family please feel free to sign-up!</p>
+        </div>
+
+      </div>
     )
   }
 }
