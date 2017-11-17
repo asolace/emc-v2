76710538e71bf7968e444ccf27a449bb9ea08df5
Jack H <jackhe678@gmail.com>
2017-11-16 20:10:04 -0500

    redux added and login setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
+import reducers from './reducers'
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+    reducers, {}, applyMiddleware(reduxThunk)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+
+registerServiceWorker()
