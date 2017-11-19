fccb0fd26c1f5c40eefaccd8ab4e8f438a7efaf4
Jack H <jackhe678@gmail.com>
2017-11-18 19:31:30 -0500

    seeding files

--- routes/seeder.js
+++ routes/seeder.js
@@ -1,62 +1,620 @@
 module.exports = [
   {
-    full_name: 'user1 lastname',
+    full_name: 'Abraham Lee',
     email: 'user1@test.com',
-    phone: '2342342345',
+    phone: '917-780-5852',
     password: 'passtest'
   },
   {
-    full_name: 'user2 lastname',
+    full_name: 'Albert Chang',
     email: 'user2@test.com',
-    phone: '2342342345',
+    phone: '646-421-0188',
     password: 'passtest'
   },
   {
-    full_name: 'user3 lastname',
+    full_name: 'Alexandra Mattis',
+    email: 'alexandraakayy17@gmail.com',
+    phone: '603-686-9220',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Andy Gmb',
+    email: 'user2@test.com',
+    phone: '917-751-7177',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Anthony Carabello',
     email: 'user3@test.com',
-    phone: '2342342345',
+    phone: '347-965-4995',
     password: 'passtest'
   },
   {
-    full_name: 'user4 lastname',
+    full_name: 'Arlene Macaraeg',
+    email: 'virarme@aol.com',
+    phone: '516-603-8089',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Brian Cheong Kuan',
+    email: 'brianckuan@gmail.com',
+    phone: '718-427-4687',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Bryan Sepulveda',
     email: 'user4@test.com',
-    phone: '2342342345',
+    phone: '917.432.7721',
     password: 'passtest'
   },
   {
-    full_name: 'user5 lastname',
+    full_name: 'Chris Razo',
+    email: 'razo.christopher@gmail.com',
+    phone: '347-465-9119',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Christian Maraboli',
     email: 'user5@test.com',
-    phone: '2342342345',
+    phone: '516-451-5090',
     password: 'passtest'
   },
   {
-    full_name: 'user6 lastname',
+    full_name: 'Christine Qian',
+    email: 'christineroe27@gmail.com',
+    phone: '347-551-3953',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Christopher Kim',
+    email: 'chriskim108@gmail.com',
+    phone: '646-592-1995',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Chunkit Lam',
+    email: 'imchunkit@gmail.com',
+    phone: '646-684-0730',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Clara Hahn',
+    email: 'clarahahn95@gmail.com',
+    phone: '646-724-7944',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Curtis Martin',
+    email: 'jiggerman22002@yahoo.com',
+    phone: '347-452-5302',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Cynthia Mikyung Cho',
+    email: 'mkcho1225@gmail.com',
+    phone: '201-233-4575',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Daniel Hsin',
+    email: 'daniel.hsin@gmail.com',
+    phone: '917-731-5423',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Daniel Mendoza',
+    email: 'superdanmendoza@gmail.com',
+    phone: '347-502-9786',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Danielle Shantol Connolly',
+    email: 'dsconnolly06@gmail.com',
+    phone: '347-962-7946',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Dasom Choe',
+    email: 'dchoe623@gmail.com',
+    phone: '646-430-2761',
+    password: 'passtest'
+  },
+  {
+    full_name: 'David Jin',
+    email: 'davidjin.mailbox@gmail.com',
+    phone: '917-837-2255',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Davon Brown',
     email: 'user6@test.com',
-    phone: '2342342345',
+    phone: '929-204-2532',
     password: 'passtest'
   },
   {
-    full_name: 'user7 lastname',
+    full_name: 'Demetri Mattis',
+    email: 'demetrimattis@gmail.com',
+    phone: '347-583-0177',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Denetta Brown',
+    email: 'dbrown0328@aol.com',
+    phone: '347-336-4078',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Ellen H. Lee',
+    email: 'elle.lee@gmail.com',
+    phone: '718-813-6727',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Enrique Pinero',
+    email: 'solstrider92881@yahoo.com',
+    phone: '347.942.1434',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Enrique Pinero Sr.',
     email: 'user7@test.com',
-    phone: '2342342345',
+    phone: '718-767-0384',
     password: 'passtest'
   },
   {
-    full_name: 'user8 lastname',
-    email: 'user8@test.com',
-    phone: '2342342345',
+    full_name: 'Ernest Jin',
+    email: 'mehh.xp@gmail.com',
+    phone: '917-584-4157',
     password: 'passtest'
   },
   {
-    full_name: 'user9 lastname',
+    full_name: 'Gloria Lee',
+    email: 'gloriasonlee@hotmail.com',
+    phone: '646-573-8006',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Grace Eunjin Lee',
+    email: 'gracelee.x3@gmail.com',
+    phone: '646.620-1455',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Helen Yoon',
+    email: 'globalization77@gmail.com',
+    phone: '917-763-9322',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Henry Thai',
+    email: 'harmicben@gmail.com',
+    phone: '516-270-6849',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Hiroki Kwangsu Nakasawa',
+    email: 'kwangsu04@gmail.com',
+    phone: ' ',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Isaac Lee',
+    email: 'isaacgbf@yahoo.com',
+    phone: '516-297-2881',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jacob Lee',
+    email: 'shnylg@hotmail.com',
+    phone: '646-573-8006',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jacqueline Kuan',
+    email: 'quacki3jacki3@gmail.com',
+    phone: '917-558-3001',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jae Lee',
+    email: 'pastorjaelee@gmail.com',
+    phone: '646-431-1336',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jason Jaesung Park',
+    email: 'jspark223@gmail.com',
+    phone: '516-567-8387',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jalysa Brown',
+    email: 'peachalishious93@aol.com',
+    phone: '562-230-3436',
+    password: 'passtest'
+  },
+  {
+    full_name: 'James Lee',
+    email: 'zhihuali@hotmail.com',
+    phone: '917-701-6888',
+    password: 'passtest'
+  },
+  {
+    full_name: 'January Drednak',
+    email: 'jdnyc247@yahoo.com',
+    phone: '646-642-0072',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jeff Chen',
+    email: 'jeff051995@gmail.com',
+    phone: '347-803-3680',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jennifer Seawright',
+    email: 'user8@gmail.com',
+    phone: '347-683-0786',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jerry Ho',
+    email: 'mrjerryho@gmail.com',
+    phone: '347-493-7957',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jessica Hjagi',
+    email: 'bintalnoor@verizon.net',
+    phone: '718-801-9524',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jhanelle Brown',
+    email: 'jhanellebrown96@gmail.com',
+    phone: '347-876-3415',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jisung Shon',
+    email: 'jshon08@gmail.com',
+    phone: '646-701-3414',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Joanne Lu',
+    email: 'jojodarealdeal08@yahoo.com',
+    phone: '919-943-1247',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jonathan Chen',
+    email: 'jchen1354@gmail.com',
+    phone: '646-240-2239',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Joon Chun',
+    email: 'jmin0105@gmail.com',
+    phone: '646-207-3339',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Joseph Lee',
+    email: 'joseph.l.lee@hotmail.com',
+    phone: '917-859-0639',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Joseph Young',
+    email: 'jyoung2452@yahoo.com',
+    phone: '718-690-0580',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Josie Du',
+    email: 'harmicben',
+    phone: '646-462-9154',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Julian Jung',
+    email: 'jjung28@binghamton.edu',
+    phone: '347-652-7130',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Julie Kim',
+    email: 'hijuliekim@yahoo.com',
+    phone: '917-664-5826',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Jungyo Kim',
+    email: 'jykim831@gmail.com',
+    phone: '646-243-2472',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Kaylee Hahn',
     email: 'user9@test.com',
-    phone: '2342342345',
+    phone: '917-593-9339',
     password: 'passtest'
   },
   {
-    full_name: 'user10 lastname',
+    full_name: 'Kenneth Siu',
     email: 'user10@test.com',
-    phone: '2342342345',
+    phone: '718-619-6988',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Kenny Jin',
+    email: 'kennyjin.dsp@gmail.com',
+    phone: '917-836-9009',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Kevin Jung',
+    email: 'user11@test.com',
+    phone: '347-707-3374',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Kihyun (James) Kim',
+    email: 'jk744744@icloud.com',
+    phone: '646-353-4475',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Kay (Kyong Sil) Choi',
+    email: 'yeohang@msn.com',
+    phone: '347-776-1515',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Lance Cho',
+    email: 'lance.cho13@stjohns.edu',
+    phone: '917-334-0904',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Lee Zhu',
+    email: 'user12@test.com',
+    phone: '718-326-7786',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Lily Ramos',
+    email: 'ramosliw@yahoo.com',
+    phone: '718-468-9071',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Liye Zhou',
+    email: 'liyezhou90@gmail.com',
+    phone: '747-272-3472',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Lulu Lim',
+    email: 'luvhope58@gmail.com',
+    phone: '718-508-2339',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Maritza Pinero',
+    email: 'martina1947@yahoo.com',
+    phone: '929-422-5863',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Matthew Gao',
+    email: 'gao.matt@gmail.com',
+    phone: '917-945-7886',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Melissa Macaraeg',
+    email: 'mellymac318@gmail.com',
+    phone: '516-603-3177',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Michael Cho',
+    email: 'mkcho0621@gmail.com',
+    phone: '201-233-2098',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Mike Kim',
+    email: 'michol_kim@hotmail.com',
+    phone: '347-840-1893',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Mimi Ahn',
+    email: 'mimiahn68@hotmail.com',
+    phone: '718-887-6203',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Natalie Biceno',
+    email: 'nataliebriceno@aim.com',
+    phone: '347-701-9495',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Ngar May Shirley Hsin',
+    email: 'shirleync683@msn.com',
+    phone: '917-225-7734',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Nilsa Morales',
+    email: 'nildropesa1y@gmail.com',
+    phone: '347-777-1261',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Paul Kim',
+    email: 'paulskim89@gmail.com',
+    phone: '347-956-0209',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Paul Ranftle',
+    email: 'sebringlxi78@gmail.com',
+    phone: '718-736-4932',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Peggy Kim',
+    email: 'peggyt327@gmail.com',
+    phone: '917-886-2411',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Philip Park',
+    email: 'idyllicny@gmail.com',
+    phone: '917-696-9349',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Pio Lee',
+    email: 'ebopio@gmail.com',
+    phone: '347-804-2264',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Rae Eun Lee',
+    email: 'raelee76@gmail.com',
+    phone: '718-506-6246',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Richard Chang',
+    email: 'user13@test.com',
+    phone: '646-330-1799',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Richard Scotty Chang',
+    email: 'user14@test.com',
+    phone: '646-770-7980',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Ruth Hahn',
+    email: 'user15@test.com',
+    phone: '917-216-3261',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Sam Gilnam  Choi',
+    email: 'choisamuel95@gmail.com',
+    phone: '347-608-7222',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Samsung Lee',
+    email: 'user16@test.com',
+    phone: '646-670-1004',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Sarah Jin',
+    email: 'user17@test.com',
+    phone: '914-623-2049',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Sherry Thai',
+    email: 'theshieldoffaith@gmail.com',
+    phone: '516-209-7052',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Skyler Hsiu',
+    email: 'iflyfreejoy@yahoo.com',
+    phone: '646-513-1494',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Soo-Il Lee',
+    email: 'leesmindcare@gmail.com',
+    phone: '516-395-8706',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Stefanie Shih',
+    email: 'stefanieshih@hotmail.com',
+    phone: '646-675-2383',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Stephen Arena',
+    email: 'user18@test.com',
+    phone: '516-507-2215',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Susie Cho',
+    email: 'susiecho620@gmail.com',
+    phone: ' ',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Susie Im',
+    email: 'susiepark222@gmail.com',
+    phone: '347-533-0966',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Thomas Rossilli',
+    email: 'trpatriots8@gmail.com',
+    phone: '631-944-2654',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Timothy Kim',
+    email: 'teekaytasks@gmail.com',
+    phone: '347-804-6551',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Tony Ward',
+    email: 'tone1prod@aol.com',
+    phone: '718-926-6886',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Victor DeSouza',
+    email: 'kyo.desouza@gmail.com',
+    phone: '347-257-6309',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Victor Maraboli',
+    email: 'user19@test.com',
+    phone: '631-355-4661',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Vincent Micelli',
+    email: 'user20@test.com',
+    phone: '347-860-2642',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Virgil Macaraeg',
+    email: 'gileph316@yahoo.com',
+    phone: '516-603-3183',
+    password: 'passtest'
+  },
+  {
+    full_name: 'Yuchang Kuo',
+    email: 'yck1126@gmail.com',
+    phone: '917-345-9196',
     password: 'passtest'
   }
 ]
